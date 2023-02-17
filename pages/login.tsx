a39ac3e04af7e623b324da6a1548b7d101a8d7b8
D-Valentin <dumitracheedi2@gmail.com>
2023-02-17 18:48:50 +0200

    code add in login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,8 +2,26 @@ import * as React from "react";
 
 function LoginPage() {
     return (
-        <div>
-            <h1 className="">Welcome to my Login Page!</h1>
+
+        <div className=" grid place-items-center mt-20">             
+
+            <div className="bg-red-200 border-2 leading-9 w-80  h-120  pl-2.5">
+                <form action="api/login" method="POST">
+                    <h1 className="text-center mt-5">Login Here</h1> <br /> 
+
+                    <label htmlFor="Email">Email</label> <br />
+                    <input type="text" required /> <br />
+
+                    <label htmlFor="Password">Password</label> <br /> 
+                    <input type="text" required /> <br />
+                    <div >
+                        <button>Google</button>
+                        <button>Facebook</button>
+                    </div>
+                    <button className="block mx-auto my-6 border bg-red-300 px-3.5" type="submit">Log in</button>
+                </form>
+            </div>
+
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
         // Or if using `src` directory:
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
+    darkMode : 'class',
     theme: {
         extend: {},
     },
     plugins: [],
+
+    
 };
