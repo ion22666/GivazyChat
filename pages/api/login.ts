87f72484f08b61b5875de0ccd90501f22244163e
ion22666 <ionmocanu2266@gmail.com>
2023-02-17 11:53:59 +0200

    corectat structura la login handler

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,5 +1,7 @@
 import type { NextApiHandler } from "next";
 
-export const handler: NextApiHandler = (req, res) => {
+const handler: NextApiHandler = (req, res) => {
     res.status(200).json({ message: "This is the '/login' POST handler" });
 };
+
+export default handler;
