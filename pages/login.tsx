247a011c8c1c739aae84735d03b4c2c8a8043785
D-Valentin <dumitracheedi2@gmail.com>
2023-02-19 16:44:24 +0200

    added at login.tsx and register.tsx next router and delete anchor tags

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,54 +1,92 @@
 import * as React from "react";
-
-import { useRouter } from 'next/router'
-
+import { useRouter } from "next/router";
 
 function LoginPage() {
+  const router = useRouter();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [formError, setFormError] = React.useState("");
 
-   
-    const router = useRouter();
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const handleButtonClick = () => {
-        router.push('/register')
-        
-        }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    return (
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (!email) {
+      setFormError("Email not provided");
+      return;
+    }
+    if (!password) {
+      setFormError("Password not provided");
+      return;
+    }
+    // Submit form
+    setFormError("");
+    router.push("/api/login");
+  };
 
-        <div className=" grid place-items-center mt-20">             
+  const handleButtonClick = () => {
+    router.push("/register");
+  };
 
-            <div className=" bg-Black-Blue border-2 leading-9 w-80  h-120 px-2.5 rounded-md ">
-                <form action="api/login" method="POST" encType="application/x-www-form-urlencoded">
-
-                    <h1 className="text-center mt-5 text-xl  font-semibold text-white   ">Login</h1> <br /> 
-
-                    <label className="text-white" htmlFor="Email ">Email</label> <br />
-                    <input className="w-full rounded" type="text" required /> <br />
-
-                    <label className="text-white" htmlFor="Password">Password</label> <br /> 
-                    <input className="w-full rounded" type="password" required /> <br />
-                    <button className="bg-white mx-auto my-4 border px-3.5 w-full rounded font-semibold text-Black-Blue" type="submit" >LOGIN</button>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <button className="border bg-red-300 rounded-md ">Google</button>
-                        <button className="border bg-red-300  rounded-md ">Facebook</button>
-                    </div>
-
-
-                    <button onClick={handleButtonClick}  className= ' text-white block  mb-2.5 mx-auto my-auto  '>Register</button>
-                    
-                </form>
-            </div>
-
-        </div>
-
-
-        
-    );
+  return (
+    <div className=" grid place-items-center mt-20">
+      <div className=" bg-Black-Blue border-2 leading-9 w-80  h-120 px-2.5 rounded-md ">
+        <form onSubmit={handleFormSubmit}>
+          <h1 className="text-center mt-5 text-xl  font-semibold text-white   ">
+            Login
+          </h1>{" "}
+          <br />
+          <label className="text-white" htmlFor="email">
+            Email
+          </label>{" "}
+          <br />
+          <input
+            id="email"
+            className="w-full rounded"
+            type="text"
+            value={email}
+            onChange={handleEmailChange}
+          />{" "}
+          <br />
+          <label className="text-white" htmlFor="password">
+            Password
+          </label>{" "}
+          <br />
+          <input
+            id="password"
+            className="w-full rounded"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />{" "}
+          <br />
+          {formError && <p className="text-red-600">{formError}</p>}
+          <button
+            className="bg-white mx-auto my-4 border px-3.5 w-full rounded font-semibold text-Black-Blue"
+            type="submit"
+          >
+            LOGIN
+          </button>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <button className="border bg-red-300 rounded-md ">Google</button>
+            <button className="border bg-red-300  rounded-md ">Facebook</button>
+          </div>
+          <button
+            onClick={handleButtonClick}
+            className=" text-white block  mb-2.5 mx-auto my-auto  "
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
-
-
-//background-color: #ececec;
-//background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23000000' fill-opacity='0.29'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
-
 
 export default LoginPage;
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,56 +1,129 @@
 import * as React from "react";
 
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
+import { useRef, useState } from "react";
 
+// next.js route function to login page
 
-
-function RegisterPage(){
-
+function RegisterPage() {
     const router = useRouter();
 
     const handleButtonClick = () => {
-        router.push('/login')
-        
-        }  
+        router.push("/login");
+    };
 
-    return(
-        <div className=" grid place-items-center mt-10">             
+// Verificator de  input pentru parole
 
+    const passwordRef = useRef(null);
+    const confirmPasswordRef = useRef(null);
+    const [passwordError, setPasswordError] = useState(false);
+    const [passwordsMatch, setPasswordsMatch] = useState(false);
+    const [passwordLengthError, setPasswordLengthError] = useState(false);
+
+    const handleInputChange = () => {
+        if (passwordRef.current.value === confirmPasswordRef.current.value) {
+          setPasswordsMatch(true);
+          setPasswordError(false);
+        } else {
+          setPasswordsMatch(false);
+          setPasswordError(true);
+        }
+      
+        if (passwordRef.current.value.length < 8) {
+          setPasswordLengthError(true);
+        } else {
+          setPasswordLengthError(false);
+        }
+      };
+
+// retrun cu formularu de register
+
+    return (
+        <div className=" grid place-items-center mt-10">
             <div className=" bg-Black-Blue border-2 leading-9 w-80  h-120 px-2.5 rounded-md ">
-                <form action="api/login" method="POST" encType="application/x-www-form-urlencoded">
+                <form
+                    action="api/login"
+                    method="POST"
+                    encType="application/x-www-form-urlencoded"
+                >
+                    <h1 className="text-center mt-5 text-xl  font-semibold text-white   ">
+                        Register
+                    </h1>{" "}
+                    <br />
+                    <label className="text-white" htmlFor="Email ">
+                        Email
+                    </label>{" "}
+                    <br />
+                    <input
+                        className="w-full rounded"
+                        type="email"
+                        required
+                    />{" "}
+                    <br />
+                    <label className="text-white" htmlFor=" username ">
+                        Username
+                    </label>{" "}
+                    <br />
+                    <input
+                        className="w-full rounded"
+                        type="text"
+                        required
+                    />{" "}
+                    <br />
+                    <label className="text-white" htmlFor="Password">
+                        Password
+                    </label>{" "}
+                    <br />
+                    <input
+                        className="w-full rounded"
+                        type="password"
+                        required
+                        ref={passwordRef}
+                        onChange={handleInputChange}
+                    />{" "}
+                    <br />
+                    <label className="text-white" htmlFor="password">
+                        {" "}
+                        Confirm Password
+                    </label>{" "}
+                    <br />
+                    <input
+                        className="w-full rounded"
+                        type="password"
+                        required
+                        ref={confirmPasswordRef}
+                        onChange={handleInputChange}
+                    />{" "}
+                    <br />
+                    {passwordLengthError && <p className="text-red-600">Parola trebuie sa fie de minim 8 caractere</p>}
+                    {passwordError && <p className="text-red-600">Parolele nu coincid!</p>}
+                    <button
 
-                    <h1 className="text-center mt-5 text-xl  font-semibold text-white   ">Register</h1> <br /> 
-
-                    <label className="text-white" htmlFor="Email ">Email</label> <br />
-                    <input className="w-full rounded" type="text" required /> <br />
-
-                    <label className="text-white" htmlFor=" username ">Username</label> <br />
-                    <input className="w-full rounded" type="text" required /> <br />
-
-                    <label className="text-white" htmlFor="Password">Password</label> <br /> 
-                    <input className="w-full rounded" type="password" required  /> <br />
-
-                    <label className="text-white" htmlFor="password"> Confirm Password</label> <br /> 
-                    <input className="w-full rounded" type="password" required  /> <br />
-
-
-                    <button className="bg-white mx-auto border px-3.5 w-full rounded font-semibold text-Black-Blue my-5" type="submit" >Register</button>
+                        disabled={!passwordsMatch || passwordLengthError}
+                        className="bg-white mx-auto border px-3.5 w-full rounded font-semibold text-Black-Blue my-5"
+                        type="submit"
+                    >
+                        Register
+                    </button>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <button className="border bg-red-300 rounded-md ">Google</button>
-                        <button className="border bg-red-300  rounded-md ">Facebook</button>
+                        <button className="border bg-red-300 rounded-md ">
+                            Google
+                        </button>
+                        <button className="border bg-red-300  rounded-md ">
+                            Facebook
+                        </button>
                     </div>
-                    <button onClick={handleButtonClick}  className= ' text-white block  mb-2.5 mx-auto my-auto  '>Register</button>
-                    
+                    <button
+                        onClick={handleButtonClick}
+                        className=" text-white block  mb-2.5 mx-auto my-auto  "
+                    >
+                        Login
+                    </button>
                 </form>
             </div>
-
         </div>
-
-
-        
     );
-    
 }
 
 export default RegisterPage;
