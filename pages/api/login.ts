5f5aeafe34c25be12377ee02db09355e9e630eef
Zyppon <mcpezyppon@gmail.com>
2023-02-20 21:25:49 +0100

    modified login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,20 @@
 import type { NextApiHandler } from "next";
 
-import { User } from "../../models/user";
+const handler: NextApiHandler = (req, res) => {
+    res.status(200).json({ message: "This is the '/login' POST handler" });
 
-const handler: NextApiHandler = async (req, res) => {
-    // try {
-    //     await User.create({ email: "iulica@gmail.com" });
-    // } catch (e) {
-    //     return res.status(500).json({ error: e });
-    // }
+    if (req.method === 'POST') {
+          const email = req.body.email;
+          const password = req.body.password;
+          if (email === 'emailcorect@example.com' && password === 'parolacorecta') {
+            res.status(200).json({ message: 'Autentificare reușită' });
+          } else {
+            res.status(401).json({ message: 'Email sau parolă incorecte' });
+          }
+        } else {
+          res.status(405).json({ message: 'Metoda HTTP nu este permisă' });
+        }
+      }
 
-    // return all users from the database
-    return res.status(200).json({ message: "This is the LoginPage API Handler", users: await User.find() });
-};
 
 export default handler;
