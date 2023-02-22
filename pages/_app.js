1dbc25492eeddc140a7149ed17ebfaeb76e01841
Valentin <dumitracheedi2@gmail.com>
2023-02-22 09:38:31 +0200

    added loading screen and loading.tsx, modified _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import "./styles.css";
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+import React from 'react'
+import Router from "next/router";
+import Loading from "../pages/loading";
+
+function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = React.useState(false);
+
+  Router.events.on("routeChangeStart", () => setLoading(true));
+  Router.events.on("routeChangeComplete", () => {
+    setTimeout(() => setLoading(false), 1050); // Adăugați o întârziere de 2.5 secunde
+  });
+
+  return (
+    <>
+      {loading && <Loading />}
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+
+export default MyApp;
