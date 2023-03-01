577d294628d3391bf452402cef9fa2006ed72653
ion22666 <ionmocanu2266@gmail.com>
2023-03-01 19:18:35 +0200

    shema la home page, inspirat dupa discord

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html lang="en">
-            <Head />
-            <body>
+        <Html lang="en" className="w-full h-full overflow-hidden">
+            <Head>
+                <style>{`
+                    #__next{
+                        height:100%;
+                        width:100%;
+                        overflow:hidden;
+                    }
+                `}</style>
+            </Head>
+            <body className="w-full h-full overflow-hidden">
                 <Main />
                 <NextScript />
             </body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 
+const images = ["https://static.deepl.com/img/logo/DeepL_Logo_darkBlue_v2.svg"].map(e => [e, e, e, e, e, e, e, e, e]);
+images[0][0] = "img/20230215_183612.png";
 function HomePage() {
     return (
-        <div>
-            <h1 className="bg-red-500">Welcome to my Next.js app!</h1>
+        <div className="h-full w-full flex flex-row bg-black">
+            <div className="bg-neutral-600 flex flex-col gap-1 p-1">
+                {images[0].map(image => (
+                    <img src={image} className="w-12 aspect-square" />
+                ))}
+            </div>
+            <div className="bg-neutral-700 w-72"></div>
+            <div className="flex-grow bg-neutral-800"></div>
         </div>
     );
 }
