64f82c29b3330566ffb17a309a18e7c5c0b73cef
ion22666 <ionmocanu2266@gmail.com>
2023-03-05 15:31:01 +0200

    deleted old database,models and api files

--- database/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    DATABASE: "givazy",
-    USERNAME: process.env.MONGODB_USERNAME,
-    PASSWORD: process.env.MONGODB_PASSWORD,
-};
--- database/mongodb.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import config from "./config";
-
-mongoose.set("strictQuery", true);
-mongoose.connect(`mongodb+srv://${config.USERNAME}:${config.PASSWORD}@cluster0.wgurxzm.mongodb.net/${config.DATABASE}?retryWrites=true&w=majority`);
-
-export default mongoose;
