99bf24541be4d38df04424346863548d44834b56
ion22666 <ionmocanu2266@gmail.com>
2023-03-10 19:56:04 +0200

    am bagat comentarii si aranjat mai bine lucrurile

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -5,34 +5,48 @@ import { AppContext } from "../pages";
 import Message from "./Message";
 
 const Chat: React.FunctionComponent = () => {
-    const { userData, activeChat, userFriendsData, socket } = React.useContext(AppContext);
-    const obj = React.useContext(AppContext);
-    if (!(userData && activeChat && userFriendsData)) return <div></div>;
+    let { userData, activeChat, userFriendsData, chats, socket } = React.useContext(AppContext);
 
-    const inputRef = React.useRef<HTMLInputElement>();
+    // avem nevoie de obiectul 'AppContext' pentru a putea folosi 'AppObj.forceReRenderChat()'
+    // deorece 'forceReRenderChat()' foloseste 'this'
+    const AppObj = React.useContext(AppContext);
 
+    // cand pagina de chat este on screen, conectam clientul la serverul ws pentru a putea primi mesaje
     React.useEffect(() => {
         socket.on("push message", (message: global.Message, chat_id) => {
             if (activeChat._id === chat_id) {
                 activeChat.messages.push(message);
-                obj.forceReRenderChat();
+                AppObj.forceReRenderChat();
             }
         });
         return () => {
             socket.off("push message");
         };
     }, []);
+
+    // fara userData && userFriendsData pagina de chat nu poate exista
+    if (!(userData && userFriendsData)) return <div>{"Loading..."}</div>;
+
+    if (chats.length === 0) return <div>{"You have no friends :("}</div>;
+
+    // daca nu exista inca un activeChat, il folosim pe primul din lista ( nu e nevoie sa folosim 'setChats()' )
+    if (!activeChat) activeChat = chats[0];
+
+    const inputRef = React.useRef<HTMLInputElement>();
+
     function sendMessage() {
         const partial_message: global.PartialMessage = {
             content: inputRef.current.value,
         };
         socket.emit("send message", partial_message, activeChat._id);
     }
+
+    // gasim userul la care se refera activeChat-ul din lista 'userFriendsData'
     const friendData = userFriendsData.find(e => e._id === userData.friends.find(e => e.chatId === activeChat._id).friendId);
 
     return (
-        // <DesktopChatComponent 
-        <div className="w-full h-full flex flex-col place-content-stretch">
+        // <DesktopChatComponent
+        <div className="flex h-full w-full flex-col place-content-stretch">
             {/* Asta e containeru cu mesaje */}
             <div className="w-full flex-grow overflow-y-auto overflow-x-hidden">
                 {activeChat.messages.map(message => (
@@ -42,16 +56,16 @@ const Chat: React.FunctionComponent = () => {
                 <div style={{ height: "100vh" }}></div>
             </div>
             {/* Asta e containeru de jos care are inputul */}
-            <div className="h-16 w-full flex flex-row gap-1 px-2 py-3">
+            <div className="flex h-16 w-full flex-row gap-1 px-2 py-3">
                 <input
                     ref={inputRef}
                     placeholder="Send a message"
                     type="text"
-                    className="bg-stone-700 font-Whyte-Book flex-grow rounded-xl py-3 px-2 text-lg focus:outline-Verde focus:outline-2 focus:outline"
+                    className="flex-grow rounded-xl bg-stone-700 py-3 px-2 font-Whyte-Book text-lg focus:outline focus:outline-2 focus:outline-Verde"
                 />
                 {/* asta e containerul la SendIcon, e mai bine cand pui iconitele intrun wrapper/container decat sa le pui direct asa, iti permite sa faci animatii pe ele mai usor */}
-                <div onClick={sendMessage} className="h-full flex justify-center items-center">
-                    <SendIcon className="h-full aspect-square hover:p-1 text-Verde hover:text-orange-400 duration-100 ease-linear active:p-2 active:opacity-100" />
+                <div onClick={sendMessage} className="flex h-full items-center justify-center">
+                    <SendIcon className="aspect-square h-full text-Verde duration-100 ease-linear hover:p-1 hover:text-orange-400 active:p-2 active:opacity-100" />
                 </div>
             </div>
         </div>
